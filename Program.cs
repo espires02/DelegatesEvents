d8a7d33f9504579f2961f5aa9355c1a2e6104839
eleaz <eleaz@DESKTOP-ALM5Q8D>
2024-04-24 20:03:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace delegatesAndEvents
+{
+    // create a delegate
+    public delegate void myDelegate(int n);
+    public class Race
+    {
+        // create a delegate event object
+        public event myDelegate raceWon;
+        public void Racing(int contestants, int laps)
+        {
+            Console.WriteLine("Ready\nSet\nGo!");
+            Random r = new Random();
+            int[] participants = new int[contestants];
+            bool done = false;
+            int champ = -1;
+            // first to finish specified number of laps wins
+            while (!done)
+            {
+                for (int i = 0; i < contestants; i++)
+                {
+
+                    if (participants[i] <= laps)
+                    {
+                        participants[i] += r.Next(1, 5);
+                    }
+                    else
+                    {
+                        champ = i;
+                        done = true;
+                        continue;
+                    }
+                }
+
+            }
+
+            TheWinner(champ);
+        }
+        private void TheWinner(int champ)
+        {
+            Console.WriteLine("We have a winner!");
+            // invoke the delegate event object and pass champ to the method
+            raceWon(champ);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            // create a class object
+            Race round1 = new Race();
+            // register with the footRace event
+            round1.raceWon += footRace;
+            // trigger the event
+            round1.Racing(5, 1);
+            // register with the carRace event
+            round1.raceWon-= footRace;
+            round1.raceWon += carRace;
+            //trigger the event
+            round1.Racing(10, 15);
+            // register a bike race event using a lambda expression
+            round1.raceWon-= carRace;
+            round1.raceWon += (winner) => Console.WriteLine($"The bike race winner is number {winner}.");
+            // trigger the event
+            round1.Racing(12, 5);
+        }
+
+        // event handlers
+        public static void carRace(int winner)
+        {
+            Console.WriteLine($"Car number {winner} is the winner.");
+        }
+        public static void footRace(int winner)
+        {
+            Console.WriteLine($"Racer number {winner} is the winner.");
+        }
+    }
+}
